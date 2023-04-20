5516e8a74c9dd4d8a46ce37a3791ec2e1c4dc8f5
Harshit Sachan <harshitsachan99@gmail.com>
2023-04-20 12:20:36 +0530

    RCT Updated

---
+++ task1/solution1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://e7.pngegg.com/pngimages/791/922/png-clipart-react-javascript-library-redux-user-interface-tesseract-logo-symmetry.png" alt="logo" width="150px" style={{borderRadius:"50%"}}/>
+
+      <h2>Benefits of using React...</h2>
+
+<ol>
+  <li>Component-based architecture</li>
+  <li>Visuial Dom for efficient updates</li>
+  <li>Rich ecosystem and community</li>
+  <li>Cross-platform development</li>
+  <li>Strong community support</li>
+</ol>
+<button>Get Started...</button>
+    </div>
+  );
+}
+
+export default App;
